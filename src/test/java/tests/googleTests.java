477407e420ce441b61ad57ef224269606c79c124
Mikhail_Mironov <MiLLIan91@mail.ru>
2021-02-21 17:02:04 +0400

    Добавлена рыба первого теста

---
+++ src/test/java/tests/googleTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class googleTests{
+
+    @Test
+    public void test1() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.quit();
+
+
+
+
+    }
+}
+
